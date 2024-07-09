8ccb7f42544ff1107adc872cab122f4d6647d575
Dongkyun Kim <donkim0122@gmail.com>
2024-07-09 12:29:16 +0900

    Time: 208 ms (78.79%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,13 +1,9 @@
 SELECT 
-    machine_id,
-    ROUND(SUM(process_time) / (COUNT(1) / 2), 3) AS processing_time
-FROM (
-    SELECT 
-        machine_id,
-        CASE 
-            WHEN activity_type LIKE 'end' THEN timestamp 
-            ELSE -timestamp 
-        END AS process_time
-    FROM Activity
-) AS processed_data
-GROUP BY machine_id;
+    a.machine_id AS machine_id,
+    ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+FROM Activity a
+JOIN Activity b
+    ON a.machine_id = b.machine_id
+    AND a.activity_type = 'start'
+    AND b.activity_type = 'end'
+GROUP BY 1;
