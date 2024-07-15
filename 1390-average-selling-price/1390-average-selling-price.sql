d5a7b3f8d7b0196089094134c38107f3168b91a1
Dongkyun Kim <donkim0122@gmail.com>
2024-07-15 09:31:58 +0900

    Time: 632 ms (88.42%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,14 +1,7 @@
-WITH all_products AS (
-    SELECT DISTINCT product_id
-    FROM Prices
-)
-
-SELECT ap.product_id,
+SELECT p.product_id,
     COALESCE(ROUND(SUM(us.units * p.price) / SUM(us.units), 2), 0) AS average_price
-FROM all_products ap
+FROM Prices p
 LEFT JOIN UnitsSold us
-    ON ap.product_id = us.product_id
-LEFT JOIN Prices p
-    ON ap.product_id = p.product_id
+    ON us.product_id = p.product_id
     AND us.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY 1
