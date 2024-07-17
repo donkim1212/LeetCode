9529d8edf204e995c070f45817be9c5a87034c6b
Dongkyun Kim <donkim0122@gmail.com>
2024-07-17 09:38:16 +0900

    Time: 966 ms (62.87%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -4,7 +4,7 @@ WITH total_count AS (
 )
 
 SELECT contest_id,
-    ROUND(COUNT(DISTINCT user_id) / (SELECT count FROM total_count LIMIT 1) * 100, 2) AS percentage
+    ROUND(COUNT(user_id) / (SELECT count FROM total_count LIMIT 1) * 100, 2) AS percentage
 FROM Register
 GROUP BY 1
 ORDER BY 2 DESC, 1;
