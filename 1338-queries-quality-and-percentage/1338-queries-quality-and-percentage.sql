62070db69b7558517ae80e1ecd368af1c5e7e5b0
Dongkyun Kim <donkim0122@gmail.com>
2024-07-28 05:04:54 +0900

    Time: 358 ms (48.27%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -1,26 +1,7 @@
-WITH total_counts AS (
-    SELECT query_name,
-           COUNT(*) AS total_count
-    FROM Queries
-    GROUP BY query_name
-),
-poor_counts AS (
-    SELECT query_name,
-           COUNT(*) AS poor_count
-    FROM Queries
-    WHERE rating < 3
-    GROUP BY query_name
-),
-query_quality AS (
-    SELECT query_name,
-           ROUND(AVG(rating / position), 2) AS quality
-    FROM Queries
-    GROUP BY query_name
-)
-
-SELECT q.query_name,
-       q.quality,
-       COALESCE(ROUND(p.poor_count * 100.0 / t.total_count, 2), 0) AS poor_query_percentage
-FROM query_quality AS q
-JOIN total_counts AS t ON q.query_name = t.query_name
-LEFT JOIN poor_counts AS p ON q.query_name = p.query_name;
+SELECT 
+    query_name,
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(AVG(IF(rating < 3, 1, 0)) * 100, 2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name;
